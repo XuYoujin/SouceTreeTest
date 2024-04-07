b5715a9b604e0c7f084d1f803d85608369a056d7
xyj <824284843@qq.com>
2024-04-07 11:14:33 +0800

    删除打印

--- SourceTreeTest/AppDelegate.m
+++ SourceTreeTest/AppDelegate.m
@@ -16,7 +16,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"打印输出....!");
     return YES;
 }
 
